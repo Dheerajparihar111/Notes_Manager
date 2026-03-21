c0742d116df4188fc04791b9dc80699c1d678b51
Dheerajparihar111 <parihardheeraj.p@gmail.com>
2026-03-21 23:18:12 +0530

    add and view options added

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,69 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+import java.util.*;
+import java.io.*;
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
-}
+public class Main {
+
+    static Scanner input = new Scanner(System.in);
+    static ArrayList<Note> notes = new ArrayList<>();
+
+    static final String FILE = "notes.txt";
+
+    public static void main(String[] args) {
+
+
+        int choice;
+
+        do {
+
+            System.out.println("===== NOTE MANAGER =====");
+            System.out.println("1 Add Note");
+            System.out.println("2 View Note");
+            System.out.println("3 Exit");
+
+            choice = input.nextInt();
+            input.nextLine();
+
+            switch (choice) {
+
+                case 1:
+                    addNote();
+                    break;
+                case 2:
+                    viewNotes();
+                    break;
+                case 3:
+                    System.out.println("Thank You For Using Note Manager");
+                    System.exit(0);
+            }
+
+        } while (choice != 4);
+    }
+
+
+    static void addNote() {
+
+        System.out.print("Enter note: ");
+        String text = input.nextLine();
+
+        notes.add(new Note(text));
+    }
+
+
+    static void viewNotes() {
+
+        if (notes.isEmpty()) {
+            System.out.println("No notes");
+            return;
+        }
+
+        for (int i = 0; i < notes.size(); i++) {
+
+            System.out.print(i + " : ");
+            notes.get(i).display();
+        }
+    }
+
+
+
+
+    }
